f990daec0c1b56f570f8972a10d8544f3b57abad
rafiqahsns <rafiqhsns@gmail.com>
2019-03-07 17:22:58 +0700

    crud soal sementara

--- app/Http/Controllers/SoalController.php
+++ app/Http/Controllers/SoalController.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 
 class SoalController extends Controller
 {
-    public function create(Request $request){
+    public function index(){
+
+    }
+    public function create(SoalRequest $request){
         $soal = new Soal;
         $soal->pertanyaan = $request->pertanyaan;
         $soal->jawaban=$request->jawaban;
         $soal->mapel=$request->mapel;
+        $soal->pembahasan=$request->pembahasan;
         $soal->pilihana=$request->pilihana;
         $soal->pilihanb=$request->pilihanb;
         $soal->pilihanc=$request->pilihanc;
@@ -19,8 +23,28 @@ class SoalController extends Controller
         $soal->save();
         return $soal;
     }
-    public function read(){}
     public function delete(Soal $soal){
         Soal::where('id',$soal->id)->delete();
     }
+    public function readSoal(Soal $soal){
+        return Soal::where('id',$soal->id)->get();
+        // Pastikan ga dibalikin semua
+    }
+    public function edit(){
+        
+    }
+    public function update(SoalRequest $request){
+        $soal = Soal::find($request->id);
+        $soal->pertanyaan = $request->pertanyaan;
+        $soal->jawaban=$request->jawaban;
+        $soal->mapel=$request->mapel;
+        $soal->pembahasan=$request->pembahasan;
+        $soal->pilihana=$request->pilihana;
+        $soal->pilihanb=$request->pilihanb;
+        $soal->pilihanc=$request->pilihanc;
+        $soal->pilihand=$request->pilihand;
+        $soal->pilihane=$request->pilihane;
+        $soal->save();
+        return $request->all();
+    } 
 }
