e0f6cc633f3838277c62ae84e6581782da5a167e
rafiqahsns <rafiqhsns@gmail.com>
2019-03-07 15:11:39 +0700

    migrasi

--- database/migrations/2019_03_07_064656_create_paket_soals_table.php
+++ database/migrations/2019_03_07_064656_create_paket_soals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePaketSoalsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('paket_soals', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('id_soal');
+            $table->integer('id_paket');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('paket_soals');
     }
 }
---
+++ database/migrations/2019_03_07_074904_create_soals_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('pertanyaan');
+            $table->integer('jawaban');
+            $table->string('mapel');
+            $table->text('pilihana');
+            $table->text('pilihanb');
+            $table->text('pilihanc');
+            $table->text('pilihand');
+            $table->text('pilihane');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soals');
+    }
+}
