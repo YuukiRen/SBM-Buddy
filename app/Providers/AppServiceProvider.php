1b9db252bc648696c91d2feda611fc790c9f53fd
YuukiRen <reinaldoalvin1@gmail.com>
2019-05-24 23:16:48 +0700

    first dispatch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\Request;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -12,9 +14,14 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
+        \URL::forceRootUrl(\Config::get('app.url'));
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 
     /**
@@ -24,6 +31,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'APP_URL=https://sbmbuddy.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
